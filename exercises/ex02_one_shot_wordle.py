e5f9c7af510ea6e5b86dce5265e1b87b4e58cfdc
Chris Odondi <codondi@ad.unc.edul>
2022-01-30 15:01:51 -0500

    submission ex02 #3

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,4 +1,4 @@
-"""One guess wordle game"""
+"""One guess wordle game."""
 
 __author__ = "730449161"
 
@@ -8,13 +8,15 @@ secret_word_length: int = len(secret_word)
 player_guess: str = str(input(f"What is your { secret_word_length }-letter guess? "))
 
 while len(player_guess) != secret_word_length: 
-        player_guess = str(input(f"That was not { secret_word_length } letters! Try again: "))
+    player_guess = str(input(f"That was not { secret_word_length } letters! Try again: "))
  
 
 word_index_number: int = 0
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
+
+player_guess_somewhere_else: bool = False
 
 resulting_emojis: str = ""
 if len(player_guess) == secret_word_length:
@@ -23,16 +25,16 @@ if len(player_guess) == secret_word_length:
         if index_being_tested == secret_word[word_index_number]:
             resulting_emojis = resulting_emojis + GREEN_BOX
         else:
-            player_guess_somewhere_else: bool = False
+            #player_guess_somewhere_else: bool = False
             alt_word_index_number: int = 0
-            while player_guess_somewhere_else != True and alt_word_index_number < secret_word_length:
+            while not player_guess_somewhere_else and alt_word_index_number < secret_word_length:
                 if index_being_tested == secret_word[alt_word_index_number]:
                     player_guess_somewhere_else = True
                 else:
                     alt_word_index_number += 1
-            if player_guess_somewhere_else == True:
-                resulting_emojis = resulting_emojis +YELLOW_BOX
-        if index_being_tested != secret_word[word_index_number] and player_guess_somewhere_else == False:
+            if player_guess_somewhere_else:
+                resulting_emojis = resulting_emojis + YELLOW_BOX
+        if index_being_tested != secret_word[word_index_number] and not player_guess_somewhere_else:
             resulting_emojis = resulting_emojis + WHITE_BOX
         word_index_number += 1
 
