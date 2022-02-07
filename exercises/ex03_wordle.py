1085a6353f2855c03f3d7746350e340613056488
Chris Odondi <codondi@ad.unc.edul>
2022-02-06 21:45:03 -0500

    final submission wordle ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,6 +2,7 @@
 
 __author__ = "730449161"
 
+
 def contains_char(word_being_searched: str, character_being_searched: str) -> bool:
     """When given a two strings, the first of any length, the second a single character, will return True if the single character of the second string is found in the first string."""
     assert len(character_being_searched) == 1
@@ -13,6 +14,7 @@ def contains_char(word_being_searched: str, character_being_searched: str) -> bo
     
     return False
 
+
 def emojified(player_guess: str, secret_word: str) -> str:
     """Given two strings of equal length, will return a string of emojis who's colr codifies the same."""
     assert len(player_guess) == len(secret_word)
@@ -22,7 +24,6 @@ def emojified(player_guess: str, secret_word: str) -> str:
     GREEN_BOX: str = "\U0001F7E9"
     YELLOW_BOX: str = "\U0001F7E8"
     resulting_emojis: str = ""
-    player_guess_somewhere_else: bool = False
     
     while i < len(player_guess):
         if player_guess[i] == secret_word[i]:
@@ -36,6 +37,7 @@ def emojified(player_guess: str, secret_word: str) -> str:
     
     return resulting_emojis
 
+
 def input_guess(expected_length: int) -> str:
     """Will prompt the user for a guess until one is made of the proper length. Returns the guess."""
     characters_match: bool = False
@@ -47,14 +49,14 @@ def input_guess(expected_length: int) -> str:
 
     while not characters_match:
         player_guess = str(input(f"That wasn't { expected_length } chars! Try again: "))
-        if len (player_guess) == expected_length:
+        if len(player_guess) == expected_length:
             characters_match = True
     
     return player_guess
 
+
 def main() -> None:
     """The entrypoint of the program and main game loop."""
-    
     secret_word: str = "codes"
     used_turns: int = 1
     user_won: bool = False
@@ -70,8 +72,10 @@ def main() -> None:
 
     used_turns -= 1
     if user_won:
-        print(f"You won in { used_turns }/6 turns!" )
+        print(f"You won in { used_turns }/6 turns!")
     else:
         print("X/6 - Sorry, try again tomorrow!")
+
+
 if __name__ == "__main__":
     main()
